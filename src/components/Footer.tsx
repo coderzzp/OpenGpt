c6e79c9ec351522b092679c0ce8c1adfe2c3674e
coderzzp <coderzzp@gmail.com>
2023-03-18 11:21:45 +0800

    feat: remove TantanFu

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -45,8 +45,7 @@ export function Footer() {
             </Link> */}
           </div>
           <p className="mt-6 text-sm text-slate-500 sm:mt-0">
-            Copyright &copy; {new Date().getFullYear()} TantanFu All rights
-            reserved.
+            Copyright &copy; {new Date().getFullYear()}
           </p>
         </div>
       </Container>
