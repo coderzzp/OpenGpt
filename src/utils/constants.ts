ca2e9c7c97785617c3d74f6674a9ce72be08ead4
ByteDance <zhengzhipeng@bytedance.com>
2024-01-11 15:09:05 +0800

    feat: RATE_LIMIT_COUNT 100

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,6 +4,6 @@ export const SITE_DESC =
 // 'Access a vast library of pre-built ChatGPT applications or create your own in just seconds, with OpenGPT platform.'
 
 export const MAX_TOKENS = 2000
-export const RATE_LIMIT_COUNT = 10
+export const RATE_LIMIT_COUNT = 100
 
 export const PURCHASE_URL = process.env.NEXT_PUBLIC_PURCHASE_URL
